90216d137e2af45068d5ef7840fda6e6c6443e4a
Daedaluzz <arthurbernardpereira@gmail.com>
2024-09-07 10:10:32 -0300

    Feat:mudança no texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className={styles.textContainer}>
         <h3>Eficiência com Inovação</h3>
         <h1>Inovação Sustentável, Energia Inteligente.</h1>
-        <p>Facilitando trocas sustentáveis, o SIGAS torna o comércio de energia uma experiência eficiente. Além disso, possibilita a venda descomplicada de excedentes de energia verde. Uma solução integrada para promover a sustentabilidade.</p>
+        <p>O SIGAS é uma plataforma de IA para gestão integrada de ativos sustentáveis em sistemas agrovoltaicos, otimizando o desempenho fotovoltaico e agrícola. Utiliza IoT, machine learning e blockchain para monitoramento, manutenção preditiva e gestão de comercialização em tempo real.</p>
       <div className={styles.buttons}>
         <CtaPrimario texto='Comece Agora' link='/registrar'/>
         <CtaSecundario texto='Saber Mais' link='/quem-somos'/>
--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -17,7 +17,7 @@ export default function QuemSomos() {
         <h1>Quem Somos</h1>
         <div className={styles.bloco2}>
           <h2>Sustentabilidade financeira para produtores.</h2>
-          <p>O SIGAS, ecossistema que apresenta uma proposta inovadora ao viabilizar trocas sustentáveis e  monitorar geração e consumo de energia renovável para comercialização de excedentes.</p>
+          <p>Na BIOS, somos mais que um laboratório de pesquisa, desenvolvimento e inovação — somos impulsionadores de um futuro sustentável. Nossa empresa se dedica a criar soluções que não só atendem às demandas de sustentabilidade e eficiência, mas também promovem o desenvolvimento social e econômico de nossos clientes. Com uma abordagem que transcende a tecnologia, alinhamos nossas iniciativas com os objetivos de nossos parceiros, contribuindo para um impacto positivo e duradouro. Quer saber mais? Entre em contato conosco e descubra como podemos construir juntos um amanhã melhor.</p>
         </div>
       </div>
       <div className={styles.imageSection}>
